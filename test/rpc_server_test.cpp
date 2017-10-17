88724e49018006d450fb24a5c46acf3cd16840e1
v4if <atkjest@gmail.com>
2017-10-17 20:42:53 +0800

    2017-10-17-20:42:53

---
+++ test/rpc_server_test.cpp
@@ -0,0 +1,23 @@
+//
+// Created by root on 9/29/17.
+//
+#include "../src/rpc/rpc_server.hpp"
+#include "rpc_obj.hpp"
+#include "../src/kv/db.hpp"
+
+int main() {
+    Network::EventLoop looper;
+    RPC::RpcServer serv(&looper, 8080);
+    serv.Start();
+
+    MC.atExit([&]() {
+        looper.exit();
+    });
+
+    Rpc_Obj obj;
+    serv.registerService("Rpc_Obj::Add", &obj, &Rpc_Obj::Add);
+
+    looper.loop();
+
+    return 0;
+}
